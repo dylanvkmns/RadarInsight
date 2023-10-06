5c807b7d6083a2b49166371aeb7fc4e1cf790d6f
Dylan Veekmans <dylan.veekmans@gmail.com>
2023-10-06 12:08:31 +0200

    Added testing functionality

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+import utils
+from utils import validate_date
+
+def test_validate_date():
+    assert validate_date("01/10/2023") == True
+    assert validate_date("32/12/2023") == False
+    assert validate_date("01/13/2023") == False
+    assert validate_date("01/10/3000") == False
+    assert validate_date("random_string") == False
